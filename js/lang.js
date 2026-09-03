e9d735adeca313305fc06c2e28503f9278d58b20
ell1h <elya206@icloud.com>
2026-09-03 18:11:59 +0300

    new color

--- js/lang.js
+++ js/lang.js
@@ -1,15 +1,12 @@
-// ===== Автоматическое определение языка по браузеру =====
-function setLanguage(lang){
-    document.querySelectorAll('[lang="en"]').forEach(el => {
-        el.style.display = lang === 'en' ? '' : 'none';
+function setLanguage(lang) {
+    // Находим все элементы для выбранного языка и показываем их
+    document.querySelectorAll(`[lang="${lang}"]`).forEach(el => {
+        el.style.display = 'initial'; // или 'block' / 'inline', в зависимости от верстки
     });
-    document.querySelectorAll('[lang="ru"]').forEach(el => {
-        el.style.display = lang === 'ru' ? '' : 'none';
+
+    // Находим элементы другого языка и скрываем их
+    const invertLang = lang === 'ru' ? 'en' : 'ru';
+    document.querySelectorAll(`[lang="${invertLang}"]`).forEach(el => {
+        el.style.display = 'none';
     });
 }
-
-// язык браузера пользователя, например 'ru-RU' -> 'ru'
-const browserLang = navigator.language.slice(0, 2);
-const initialLang = browserLang === 'ru' ? 'ru' : 'en';
-
-setLanguage(initialLang);
